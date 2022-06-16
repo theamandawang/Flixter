9daf877617fc4d70ede18ea889acb8b0c4232886
Amanda Wang <amandawang@fb.com>
2022-06-16 13:36:16 -0700

    Add images, implement refresh on pull

--- Flixter/MovieViewController.m
+++ Flixter/MovieViewController.m
@@ -7,14 +7,35 @@
 
 #import "MovieViewController.h"
 #import "MovieTableViewCell.h"
+#import "UIImageView+AFNetworking.h"
+
 @interface MovieViewController () <UITableViewDataSource>
 //@property (weak, nonatomic) IBOutlet UILabel *MyLabel;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (strong, nonatomic) NSArray *results;
+@property (strong, nonatomic) UIRefreshControl *refreshControl;
 @end
 
 @implementation MovieViewController
-
+- (void) beginRefresh:(UIRefreshControl *)refreshControl {
+//    [self.refreshControl beginRefreshing];
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=ed1cb9dcb86fd8882bb243d387cc1f37"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+           if (error != nil) {
+               NSLog(@"%@", [error localizedDescription]);
+           }
+           else {
+               NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+               self.results = dataDictionary[@"results"];
+               [self.tableView reloadData];
+               [self.refreshControl endRefreshing];
+           }
+       }];
+    [task resume];
+    
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
@@ -29,61 +50,44 @@
                NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
 //               NSLog(@"%@", dataDictionary);// log an object with the %@ formatter.
                self.results = dataDictionary[@"results"];
-               NSString *string2 = @"";
-//               NSLog(@"%@", self.results);
+               NSLog(@"%@", self.results);
                // TODO: Get the array of movies
                // TODO: Store the movies in a property to use elsewhere
                // TODO: Reload your table view data
-               for(int i = 0; i < self.results.count; i++){
-//                   NSLog(@"%@", results[i]);
-                   string2 = [string2 stringByAppendingString: self.results[i][@"title"]];
-                   string2 = [string2 stringByAppendingString: @"\n"];
-               }
                self.tableView.dataSource = self;
                self.tableView.rowHeight = 300;
-
                [self.tableView reloadData];
-//               self.MyLabel.text = string2;
+               self.refreshControl = [[UIRefreshControl alloc] init];
+               [self.refreshControl addTarget:self action:@selector(beginRefresh:) forControlEvents:UIControlEventValueChanged];
+               [self.tableView insertSubview:self.refreshControl atIndex:0];
+
            }
        }];
     [task resume];
 }
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-//    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
-//    cell.textLabel.text = self.results[indexPath.row][@"title"];
-    
     MovieTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MovieCell" forIndexPath:indexPath];
     NSDictionary *data = self.results[indexPath.row];
     cell.movieTitle.text = data[@"title"];
     cell.movieDescription.text = data[@"overview"];
-//    NSString *poster_path = data[@"poster_path"];
-//    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=ed1cb9dcb86fd8882bb243d387cc1f37"];
-//    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
-//    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
-//    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+    NSString *poster_path = data[@"poster_path"];
+    poster_path = [@"https://image.tmdb.org/t/p/w500" stringByAppendingString: poster_path];
+    NSURL *posterURL = [NSURL URLWithString: poster_path];
+    NSURLRequest *posterRequest = [NSURLRequest requestWithURL:posterURL cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+
+    [cell.movieImage setImageWithURL:posterURL];
+
+//    NSURLSession *posterSession = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+//    NSURLSessionDataTask *task = [posterSession dataTaskWithRequest:posterRequest completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
 //           if (error != nil) {
 //               NSLog(@"%@", [error localizedDescription]);
 //           }
 //           else {
-//               NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
-////               NSLog(@"%@", dataDictionary);// log an object with the %@ formatter.
-//               self.results = dataDictionary[@"results"];
-//               NSString *string2 = @"";
-//               self.tableView.dataSource = self;
-//               self.tableView.rowHeight = 100;
-//
-//               [self.tableView reloadData];
-////               self.MyLabel.text = string2;
+//               cell.movieImage.image = [UIImage imageWithData:data];
 //           }
 //       }];
 //    [task resume];
-    
-    
-    
-    
-    
-    
-    
     
     
     
